8a58b133a9db9a087585112e95d2bdc4f67b7d1e
shterneregen <shterneregen@gmail.com>
2020-04-21 23:23:01 +0300

    documenting responses

--- src/main/java/random/sfgrestdocsexample/web/model/BeerDto.java
+++ src/main/java/random/sfgrestdocsexample/web/model/BeerDto.java
@@ -45,8 +45,8 @@ public class BeerDto {
     @NotNull
     private BigDecimal price;
 
-//    private Integer quantityOnHand;
-    private Integer minOnHand;
-    private Integer quantityToBrew;
+    private Integer quantityOnHand;
+//    private Integer minOnHand;
+//    private Integer quantityToBrew;
 
 }
